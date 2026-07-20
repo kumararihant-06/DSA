b8088860f87ef232b3ec63863f93ac9f7062c62a
kumararihant-06 <kumararihant.iiitm@gmail.com>
2026-07-20 21:58:40 +0530

    Time: 7 ms (45.14%), Space: 20.2 MB (24.52%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -8,8 +8,8 @@ public:
             int course = prerequisite[0];
             int pre = prerequisite[1];
 
-            adj[course].push_back(pre);
-            inDegree[pre]++;
+            adj[pre].push_back(course);
+            inDegree[course]++;
         }
         for(int i=0; i<inDegree.size();i++){
             if(inDegree[i]==0) q.push(i);
