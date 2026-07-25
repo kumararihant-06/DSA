0e092a148982d44ce7b4f7b639e9522fefc10d64
kumararihant-06 <kumararihant.iiitm@gmail.com>
2026-07-25 09:42:26 +0530

    Time: 0 ms (100%), Space: 8.1 MB (19.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
+    int solve(int n, vector<int> &dp){
+        //Memoization Method TC: O(n) SC: O(n)+O(n);
+        if(n==1||n==0) return n;
+        if(dp[n]!= -1) return dp[n];
+        return dp[n] = solve(n-1,dp)+solve(n-2,dp);
+
+    }
     int fib(int n) {
-        //Recursion method
+        //Recursion method TC: O(2^n) SC: O(n)
 
         // if(n == 0) return 0;
         // if(n == 1) return 1;
@@ -9,21 +16,19 @@ public:
         // int ans2 = fib(n-2);
         // return ans1+ans2;
 
-        //Memoization Method
-        // vector<int> dp (n+1, -1);
-        // if(n==1||n==0) return n;
-        // if(dp[n]!= -1) return dp[n];
-        // return dp[n] = fib(n-1)+fib(n-2);
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+       
 
-        // Tabulation method;
-        if(n==0 || n==1) return n;
-        int prev1 = 1;
-        int prev2 = 0;
-        for (int i=2; i<=n; i++){
-            int curr = prev1+prev2;
-            prev2 = prev1;
-            prev1 = curr;
-        }
-    return prev1;
+    //     // Tabulation method; TC: O(n) SC: O(1)
+    //     if(n==0 || n==1) return n;
+    //     int prev1 = 1;
+    //     int prev2 = 0;
+    //     for (int i=2; i<=n; i++){
+    //         int curr = prev1+prev2;
+    //         prev2 = prev1;
+    //         prev1 = curr;
+    //     }
+    // return prev1;
     }
 };
