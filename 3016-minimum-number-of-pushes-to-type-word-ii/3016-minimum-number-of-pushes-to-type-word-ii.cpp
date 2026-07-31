f9fcb802b70c6d370a5282b951425c09270f1c6c
kumararihant-06 <kumararihant.iiitm@gmail.com>
2026-07-31 13:38:25 +0530

    Time: 7 ms (69.76%), Space: 28.4 MB (25.39%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     int minimumPushes(string word) {
+        // Due to duplicate characters we should find the freq of characters first 
+        // and then assign them costs.
         vector<int> freq(26,0);
         for(char ch: word){
             freq[ch-'a']++;
         }
-
+        
+        //Highest freq should get lowest cost;
         sort(freq.rbegin(),freq.rend());
 
         int answer = 0;
         for(int i = 0; i<26; i++){
+            //Character not present no need to assign any cost now
             if(freq[i]==0)break;
-
+            
+            //Assign highly frequent keys to lowest cost i.e 1 first (0-7), then 2(8-15).....
             int pushes = (i/8)+1;
 
+            //how many times we actually need to push the key.
             answer+=freq[i]*pushes;
         }
         return answer;
